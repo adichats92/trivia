dd0f4dea35e7843d29b8dca55ff4ae4bcadb5e39
Aadil Ahmad Chattha <136930460+adichats92@users.noreply.github.com>
2023-11-27 01:05:49 +0100

    almost there

--- src/components/DynamicTResult.jsx
+++ src/components/DynamicTResult.jsx
@@ -1,16 +1,43 @@
-const DynamicTResult = ({ userAnswer }) => {
+import Spinner from 'react-bootstrap/Spinner';
+const DynamicTResult = ({
+	userAnswer,
+	isLoading,
+	userScore,
+	totalQuestions,
+}) => {
 	return (
 		<div>
-			<h2>Review Answers</h2>
-
-			{userAnswer.map((q) => (
-				<div key={q.id}>
-					<h3>{q.question}</h3>
-					{q.answer === q.correctAnswer ? <p>✅</p> : <p>❌</p>}
-					<p>My Answer: {q.answer}</p>
-					<p>Correct Answer: {q.correctAnswer}</p>
+			{isLoading ? (
+				<Spinner
+					animation='border'
+					variant='primary'
+				/>
+			) : (
+				<div>
+					<h2 className='my-3 p-2'>Review Answers</h2>
+					<p className='my-3 text-success-emphasis'>{`Scored: ${userScore} out of ${totalQuestions}`}</p>
+					{userAnswer.map((q) => (
+						<div
+							className='mb-5 p-2'
+							key={q.id}
+						>
+							<h3>{q.question}</h3>
+							{q.answer === q.correctAnswer ? (
+								<div className='d-flex justify-content-center'>
+									<p className='px-2'>My Answer: </p>
+									<p className='text-success'> {q.answer}</p>
+								</div>
+							) : (
+								<div className='d-flex justify-content-center'>
+									<p className='px-2'>My Answer: </p>
+									<p className='text-danger'> {q.answer}</p>
+								</div>
+							)}
+							<p>Correct Answer: {q.correctAnswer}</p>
+						</div>
+					))}
 				</div>
-			))}
+			)}
 		</div>
 	);
 };
--- src/components/DynamicTypeForm.jsx
+++ src/components/DynamicTypeForm.jsx
@@ -2,58 +2,124 @@ import { useCallback, useEffect, useState } from 'react';
 import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import DynamicTResult from './DynamicTResult';
+import DynamicTQuiz from './DynamicTQuiz';
 
 const DynamicTypeForm = () => {
 	const [data, setData] = useState([]);
 	const [userAnswer, setUserAnswer] = useState([]);
 	const [result, setResult] = useState(false);
 	const [randomQuiz, setRandomQuiz] = useState(null);
-	// const [clickedButton, setClickedButton] = useState({});
+	const [isLoading, setIsLoading] = useState(false);
 
-	console.log(userAnswer);
+	const saveUserAnswerToLocalStorage = (data) => {
+		localStorage.setItem('userAnswer', JSON.stringify(data));
+	};
+
+	const retrieveUserAnswerFromLocalStorage = () => {
+		const storedData = localStorage.getItem('userAnswer');
+		return storedData ? JSON.parse(storedData) : [];
+	};
+
+	useEffect(() => {
+		const storedUserAnswer = retrieveUserAnswerFromLocalStorage();
+		setUserAnswer(storedUserAnswer);
+	}, []);
+
 	useEffect(() => {
 		fetchData();
 	}, [userAnswer]);
 	useEffect(() => {
+		setIsLoading((prev) => !prev);
 		if (data.length > 0) {
+			setIsLoading((prev) => !prev);
 			const randomIndex = Math.floor(Math.random() * data.length);
+			setIsLoading((prev) => !prev);
 			setRandomQuiz(data[randomIndex]);
+			setIsLoading();
 		}
 	}, [data]);
 
 	const fetchData = () => {
+		setIsLoading((prev) => !prev);
 		axios
 			.get('https://the-trivia-api.com/v2/questions')
 			.then((res) => {
+				setIsLoading((prev) => !prev);
 				setData(res.data);
+				setIsLoading();
 			})
 			.catch((er) => console.log(er));
 	};
 
 	const handleAnswers = useCallback(
 		(questionId, selectedAnswer, selectedQuestion, correctAnswer) => {
-			setUserAnswer((prevAnswers) => [
-				...prevAnswers,
-				{
-					id: [questionId],
-					answer: selectedAnswer,
-					question: selectedQuestion,
-					correctAnswer: correctAnswer,
-				},
-			]);
+			setIsLoading((prev) => !prev);
+			const isDuplicateId = userAnswer.some(
+				(answer) => answer.id[0] === questionId
+			);
+			if (!isDuplicateId) {
+				setUserAnswer((prevAnswers) => [
+					...prevAnswers,
+					{
+						id: [questionId],
+						answer: selectedAnswer,
+						question: selectedQuestion,
+						correctAnswer: correctAnswer,
+					},
+				]);
+				saveUserAnswerToLocalStorage(userAnswer);
+			}
+			setIsLoading();
 		},
-		[]
+		[userAnswer]
 	);
 
 	const handleShow = () => {
+		setIsLoading((prev) => !prev);
 		setResult(true);
+		setIsLoading();
 	};
 
 	const handleRestart = () => {
+		setIsLoading((prev) => !prev);
 		setUserAnswer([]);
 		setResult(false);
 		setData(data);
+		setRandomQuiz(null);
+		localStorage.removeItem('userAnswer');
+		setIsLoading();
 	};
+
+	const shuffleArray = (array) => {
+		const shuffledArray = [...array];
+		for (let i = shuffledArray.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[shuffledArray[i], shuffledArray[j]] = [
+				shuffledArray[j],
+				shuffledArray[i],
+			];
+		}
+		return shuffledArray;
+	};
+
+	const shuffledAnswers = randomQuiz
+		? shuffleArray([...randomQuiz.incorrectAnswers, randomQuiz.correctAnswer])
+		: [];
+
+	const calculateScore = () => {
+		let totalQuestions = userAnswer.length;
+		let userScore = 0;
+
+		userAnswer.forEach((question) => {
+			if (question.answer === question.correctAnswer) {
+				userScore++;
+			}
+		});
+
+		return { userScore, totalQuestions };
+	};
+
+	const { userScore, totalQuestions } = calculateScore();
 
 	return (
 		<div className='d-flex flex-column justify-content-center align-items-center'>
@@ -62,61 +128,23 @@ const DynamicTypeForm = () => {
 				variant='info'
 				onClick={handleRestart}
 			>
-				Reset
+				Restart
 			</Button>
 			{!result ? (
-				<div>
-					{randomQuiz && (
-						<div>
-							<h3
-								className='my-5'
-								key={randomQuiz.id}
-							>
-								{randomQuiz.question.text}
-							</h3>
-
-							<ul>
-								{randomQuiz &&
-									[
-										...randomQuiz.incorrectAnswers,
-										randomQuiz.correctAnswer,
-									].map((answer) => (
-										<li
-											style={{ listStyle: 'none', display: 'inline' }}
-											key={answer}
-										>
-											<Button
-												variant='secondary'
-												type='radio'
-												name={randomQuiz.id}
-												value={answer}
-												className='mx-2 my-3'
-												onClick={() =>
-													handleAnswers(
-														randomQuiz.id,
-														answer,
-														randomQuiz.question.text,
-														randomQuiz.correctAnswer
-													)
-												}
-											>
-												{answer}
-											</Button>
-										</li>
-									))}
-							</ul>
-							<Button
-								className='mt-3'
-								variant='success'
-								onClick={handleShow}
-							>
-								Final Result
-							</Button>
-						</div>
-					)}
-				</div>
+				<DynamicTQuiz
+					isLoading={isLoading}
+					randomQuiz={randomQuiz}
+					shuffledAnswers={shuffledAnswers}
+					handleAnswers={handleAnswers}
+					handleShow={handleShow}
+				/>
 			) : (
-				<DynamicTResult userAnswer={userAnswer} />
+				<DynamicTResult
+					userAnswer={userAnswer}
+					isLoading={isLoading}
+					userScore={userScore}
+					totalQuestions={totalQuestions}
+				/>
 			)}
 		</div>
 	);
